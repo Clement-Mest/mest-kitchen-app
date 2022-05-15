9ccebb46a84750063427fbd6a4932c49ec6c4eaa
Benedicta Frema Boamah <bfboamah9@gmail.com|>
2022-05-15 23:18:44 +0000

    updated settings page

--- lib/screens/settings_and_profile/settings_page.dart
+++ lib/screens/settings_and_profile/settings_page.dart
@@ -1,113 +1,129 @@
 import 'package:flutter/material.dart';
+import 'package:mest_kitchen_app/screens/settings_and_profile/widgets/profile_image_widget.dart';
+import 'package:mest_kitchen_app/screens/settings_and_profile/widgets/profile_tab_content_widget.dart';
+import 'package:mest_kitchen_app/screens/settings_and_profile/widgets/settings_tab_content_widget.dart';
+import 'package:mest_kitchen_app/screens/settings_and_profile/widgets/user_code_widget.dart';
+import 'package:mest_kitchen_app/static/colors.dart';
 
 class SettingsPage extends StatefulWidget {
-  const SettingsPage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const SettingsPage({Key? key}) : super(key: key);
 
   @override
   State<SettingsPage> createState() => _SettingsPageState();
 }
 
-class _SettingsPageState extends State<SettingsPage>
-    with SingleTickerProviderStateMixin {
-  late TabController _controller;
-
+class _SettingsPageState extends State<SettingsPage> {
   @override
   void initState() {
     super.initState();
-    _controller = new TabController(length: 2, vsync: this);
   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+  String currentTab = 'PROFILE';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //Creatig the appbar and back button
+      backgroundColor: Colors.white,
       appBar: AppBar(
         centerTitle: false,
         titleSpacing: 10,
         backgroundColor: Colors.white,
         elevation: 0,
         leading: Icon(
-          Icons.arrow_left,
+          Icons.arrow_back_ios,
+          size: 16,
           color: Colors.black,
         ),
-        title: Transform(
-          // Enforcing Profile to start forcefully from left side using Transform
-          transform: Matrix4.translationValues(-20.0, 0.0, 0.0),
-          child: Text(
-            "Profile",
-            style: TextStyle(color: Colors.black, fontSize: 13),
+        title: Text(
+          "Profile",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 16,
           ),
         ),
       ),
-
       body: Column(
         children: <Widget>[
           //Top is where the
           Expanded(
-              child: Column(
-                  //image and button under goes here
-                  )),
-          Padding(
-            padding: const EdgeInsets.only(top: 4.0, bottom: 4.0),
-            child: Container(
-              decoration: new BoxDecoration(color: Colors.grey),
-              child: new TabBar(
-                controller: _controller,
-                tabs: [
-                  new Tab(
-                    text: "Profile",
-                  ),
-                  new Tab(
-                    text: 'Settings',
-                  ),
-                ],
-              ),
-            ),
-          ),
-          new Expanded(
-            child: new TabBarView(
-              controller: _controller,
-              children: <Widget>[
-                //input all text that goes into settings
-                Container(
-                    decoration: new BoxDecoration(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 60,
+                ),
+                ProfileImageWidget(),
+                SizedBox(
+                  height: 16,
+                ),
+                UserCodeWidget(),
+                SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  children: [
+                    Spacer(),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          currentTab = 'PROFILE';
+                        });
+                        print(currentTab);
+                      },
+                      child: Text(
+                        "Profile",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: currentTab == 'PROFILE'
+                              ? primaryColor
+                              : Colors.grey,
+                        ),
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      "|",
+                      style: TextStyle(
                         color: Colors.grey,
-                        borderRadius: new BorderRadius.only(
-                          topLeft: const Radius.circular(20.0),
-                          topRight: const Radius.circular(20.0),
-                        )),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        //The widgets for profile go in here
-                        Text("This is a Profile View")
-                      ],
-                    )),
-                Container(
-                    decoration: new BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: new BorderRadius.only(
-                          topLeft: const Radius.circular(20.0),
-                          topRight: const Radius.circular(20.0),
-                        )),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        //The widgets for profile go in here
-                        Text("This is a Settings View")
-                      ],
-                    )),
+                        fontSize: 28,
+                        fontWeight: FontWeight.w200,
+                      ),
+                    ),
+                    Spacer(),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          currentTab = 'SETTINGS';
+                        });
+                        print(currentTab);
+                      },
+                      child: Text(
+                        "Settings",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: currentTab == 'SETTINGS'
+                              ? primaryColor
+                              : Colors.grey,
+                        ),
+                      ),
+                    ),
+                    Spacer(),
+                  ],
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Visibility(
+                  visible: currentTab == 'PROFILE',
+                  child: ProfileTabContentWidget(),
+                ),
+                Visibility(
+                  visible: currentTab == 'SETTINGS',
+                  child: SettingsTabContentWidget(),
+                ),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
