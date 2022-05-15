5408785b1bc20b906075f0989099817629e705fa
Odafe <odafe41@gmail.com>
2022-05-15 21:56:14 +0000

    create notifications class

---
+++ lib/utils/notifications.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class Notifications {
+  String? fcmToken;
+
+  getMessagingToken() async {
+    // do something
+  }
+
+  authorizeAppNotification() async {
+    // do something
+  }
+}
