791a3fbb72be1962aa83a60a48e54f16c41a844b
Uchechi Joy Eziashi <joyatmest@gmail.com>
2022-05-15 22:04:36 +0000

    feat: added function to generate fcm token

--- lib/utils/notifications.dart
+++ lib/utils/notifications.dart
@@ -2,11 +2,16 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 
 class Notifications {
   String? fcmToken;
-
+  
+  // To generate fcmToken for a particular user
   getMessagingToken() async {
-    // do something
+    try {
+      final fcmToken = await FirebaseMessaging.instance.getToken();
+      return fcmToken;
+    } catch (e) {
+      print(e);
+    }
   }
-
   authorizeAppNotification() async {
     // do something
   }
