86950a24b4a5f040a559ad4cac942034c636a2dc
ImmArkorful <e.arkorful3@gmail.com>
2022-05-15 21:34:08 +0000

    feat: setup for notifications

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mest_kitchen_app/main.dart';
+import 'package:mest_kitchen_app/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
